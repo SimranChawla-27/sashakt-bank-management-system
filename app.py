51b7b24d7840f18008d95d673c988ae46f2b8a12
Simran <simranchawla2725@gmail.com>
2026-04-02 08:28:58 +0530

    Users table created

--- app.py
+++ app.py
@@ -12,9 +12,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('DB_USER')}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
+    aadhaar = db.Column(db.String(20), nullable=False)
+    address = db.Column(db.Text, nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
 @app.route('/')
+
 def home():
+    with app.app_context():
+        db.create_all()
     return 'Bank Management System is running!'
 
 if __name__ == '__main__':
